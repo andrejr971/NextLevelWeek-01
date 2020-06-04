bd9817837bc12fb60c47ecd144d64f68309a9837
andrejr971 <andre.souza64@fatec.sp.gov.br>
2020-06-04 13:16:29 -0300

    V 1.0

---
+++ backend/src/controllers/ItemsController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import Knex from '../database/connection';
+
+class ItemsController {
+  async index(req: Request, res: Response) {
+    const items = await Knex('items').select('*');
+
+    const serializedItems = items.map((item) => ({
+      ...item,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }));
+
+    return res.json(serializedItems);
+  }
+}
+
+export default new ItemsController();
